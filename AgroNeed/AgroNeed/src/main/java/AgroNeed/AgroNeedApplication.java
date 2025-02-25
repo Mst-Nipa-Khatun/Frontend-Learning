58bd5a06f2fefc0b757006dc27a313c559b79bf9
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-02-25 16:31:16 +0600

    "Added new E-Commerce project AgroNeed"

---
+++ AgroNeed/AgroNeed/src/main/java/AgroNeed/AgroNeedApplication.java
@@ -0,0 +1,13 @@
+package AgroNeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroNeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroNeedApplication.class, args);
+	}
+
+}
