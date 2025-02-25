44112e1f9a2a6b6c89ce4ef5786b61a9291f638c
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-02-25 17:31:10 +0600

    "Added new Entity class for E-Commerce project AgroNeed"

---
+++ AgroNeed/AgroNeed/src/main/java/AgroNeed/entity/PaymentsEntity.java
@@ -0,0 +1,33 @@
+package AgroNeed.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Payments")
+public class PaymentsEntity extends BaseEntity{
+    @Column(name = "order_id")
+    private Integer orderId;
+     @Column(name = "user_id")
+    private Integer userId;
+     @Column(name = "payment_method")
+    private String paymentMethod;
+     @Column(name = "amount")
+    private Double amount;
+     @Column(name = "status")
+    private String status;
+
+//    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+//    order_id INT,
+//    user_id INT,
+//    payment_method ENUM('PayPal', 'Stripe', 'Credit Card','Cash On'),
+//    amount DECIMAL(10,2),
+//    status ENUM('pending', 'completed', 'failed') DEFAULT 'pending',
+//    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+//    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+}
+
